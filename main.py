656a9d6e721e3feca889348e19a88bd631c5d2ca
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2022-04-08 12:27:26 +0200

    Cleanup code with black

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from src.utils import dotPlot
 
 
 def main():
-	# print(readFastaMul("./data/sequences_synthetases.fasta"))
-	dotPlot("./data/sequences_synthetases.fasta")
+    # print(readFastaMul("./data/sequences_synthetases.fasta"))
+    dotPlot("./data/toaster.fasta")
+
 
 if __name__ == "__main__":
-	main()
+    main()
--- src/sequence.py
+++ src/sequence.py
@@ -1,7 +1,7 @@
 class Sequence:
-	def __init__(self, identifier: str, sequence: str) -> None:
-		self.id = identifier
-		self.sequence = sequence
+    def __init__(self, identifier: str, sequence: str) -> None:
+        self.id = identifier
+        self.sequence = sequence
 
-	def __repr__(self) -> str:
-		return f"{self.id} : {self.sequence}"
+    def __repr__(self) -> str:
+        return f"{self.id} : {self.sequence}"
