0532e55717ea8edae9afed74c39d18d91e349165
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2022-04-08 12:26:58 +0200

    Add headers

--- src/utils.py
+++ src/utils.py
@@ -16,11 +16,14 @@ from .fasta import readFastaMul
 #             l += "*" if i == j else " "
 #         print(l)
 
+
 def getMatrix(seq1: Sequence, seq2: Sequence):
-   return [[' ' if i != j else '*' for j in seq2.sequence] for i in seq1.sequence]
+    return [[" " if i != j else "*" for j in seq2.sequence] for i in seq1.sequence]
+
 
 def dotPlot(file):
-	sequences = readFastaMul(file)
-	matrix = getMatrix(sequences[0], sequences[1])
-	for i in matrix:
-		print(''.join(i))
+    sequences = readFastaMul(file)
+    matrix = getMatrix(sequences[0], sequences[1])
+    print(f" {sequences[1].sequence}")
+    for i, val in enumerate(matrix):
+        print(sequences[0].sequence[i] + "".join(val))
