899abebe004fdcb814b274a596ccf09d37158eb8
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2022-04-13 17:45:20 +0200

    Add some sequence to test and a function to save as file matrix that are too big for the terminal
    Co-authored-by: lbProg <lbProg@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # tchoupi
+
+Tout les alignements possible pour LEO et LIE
+
+        Taille 1
+
+ ┌──────┬───────┬──────┐
+ │LEO-- │ -L-EO │ --LEO│
+ │L--IE │ LIE-- │ LIE--│
+ │      │       │      │
+ │LEO-- │ L-E-O │ -L-EO│
+ │-L-IE │ -LIE- │ L-IE-│
+ │      │       │      │
+ │LEO-- │ LE-O- │ --LEO│
+ │--LIE │ --LIE │ LI--E│
+ └──────┴───────┴──────┘
+
+        Taille 2
+
+    ┌──────┬──────┐
+    │ LEO- │ LEO- │
+    │ LI-E │ LI-E │
+    │      │      │
+    │ LEO- │ -LEO │
+    │ -LIE │ L-IE │
+    └──────┴──────┘
+
+        Taille 3
+
+        ┌─────┐
+        │ LEO │
+        │ LIE │
+        └─────┘
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from src.alignement import Alignment
-from src.fasta import readFastaMul
-from src.utils import dotPlot, drawMatrix
+from src.fasta import readFasta, readFastaMul
+from src.utils import dotPlot, drawMatrix, saveMatrix
 
 
 def main():
-    # print(readFastaMul("./data/sequences_synthetases.fasta"))
+    _, hba_Human = readFasta("data/HBA_HUMAN.fasta")
+    _, lgb2_luplu = readFasta("data/LGB2_LUPLU.fasta")
     dotPlot("./data/toaster.fasta")
 
-    ali = Alignment("LAFLALMEE", "LAME", match=0, mismatch=-1, gap=-2)
-    ali.NWSIterFill()
-
-    drawMatrix(ali.matScores)
-    drawMatrix(ali.matDir)
+    ali = Alignment(hba_Human.sequence, lgb2_luplu.sequence,  match=0, mismatch=-1, gap=-7)
+    ali.NWSIterFill(useBlosum=True)
     ali.NWSBacktrack()
+
+    saveMatrix(ali.matScores, "./data/ali.fasta.matDir")
     print(ali)
 
 
